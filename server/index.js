84e5ff7ae136718286a095bd77ef39e3f881669d
Christopher Booth <booth.christopherm@gmail.com>
2021-03-22 13:05:02 -0500

    (Modify) node server to use new email route and dependencies

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+
+//Routes
+const { emailRoute } = require('./routes/email');
 
 const public = path.join(__dirname, "../public");
 const html = path.join(public, "index.html");
 
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.static(public));
+
+//use routes
+app.use('/email', emailRoute)
 
 app.get('*', (req, res) => {
   res.sendFile(html)
