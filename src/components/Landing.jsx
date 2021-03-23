b6dfa5b5a8c76cdd9a6cbc5b8645f65ea5eef440
Christopher Booth <booth.christopherm@gmail.com>
2021-03-23 01:23:14 -0500

    (Style) landing component

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
+import Developer from '../images/developer.svg';
+import '../styles/Landing.scss'
 const Landing = () => {
   return (
     <div className="content">
-      <h2>Hi, I'm Christopher Booth</h2>
-      <h3>Welcome to my website</h3>
+      <div className="landing">
+        <div className='text-column'>
+          <h1>Hi, <br/> I'm Chris</h1>
+          <h3>full stack web developer</h3>
+          <Link to="/about"><button className="btn"><h4>Get to Know Me</h4></button></Link>
+        </div>
+        <div className="image-column">
+          <Developer />
+        </div>
+      </div>
     </div>
   )
 }
