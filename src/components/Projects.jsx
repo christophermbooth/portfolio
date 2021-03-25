dd71452e820339f1199bf3c7a6f2e791b25977e3
Christopher Booth <61816572+christophermbooth@users.noreply.github.com>
2021-03-25 14:33:30 -0500

    (Style) projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import ProjectCard from './ProjectCard';
-import {projectsData} from '../data/projects.js';
+import { projectsData } from '../data/projects.js';
+import '../styles/Projects.scss';
 
 const Projects = () => {
   return (
     <div className="content">
-      {projectsData.map(({ Name, Image, GitHub, Description }) => {
-        return <ProjectCard
-          key={Name}
-          Name={Name}
-          Image={Image}
-          GitHub={GitHub}
-          Description={Description}
-        />
-      })}
+      <div className="projects">
+        {projectsData.map(({ Name, Image, GitHub, Description, Tech }) => {
+          return <ProjectCard
+            key={Name}
+            Name={Name}
+            Image={Image}
+            GitHub={GitHub}
+            Description={Description}
+            Tech={Tech}
+          />
+        })}
+      </div>
     </div>
   )
 }
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,26 +1,34 @@
+import Prost from "../images/Projects/Prost.png";
+import Radma from '../images/Projects/Radma.png';
+import ProgrammingBooth from '../images/Projects/ProgrammingBooth.png';
+
 export const projectsData = [
   {
     Name: "Prost",
-    Image: "image",
+    Image: Prost,
     GitHub: "https://github.com/christophermbooth/Prost",
-    Description: "Prost aims to create safe drinking environments. Users can go solo or create parties and place themselves on our map to be seen by the public or only their friends. Privacy is a big concern here and users have complete control over who they share their location with. Users can create a friends list, plan events through private messages, check their area for new spots to frequent or curate a list of their favorite watering holes, view the varying status of any of the participating bars and restaurants including population, indoor / outdoor, masks or no masks, etc. On the other hand, if you are a business owner, register with us and experience a new, fluid method of communication with your customers. Alert them to promotions, popup events, crowd numbers, and keep them up to date about the going-ons in your business."
+    Description: "Prost aims to create safe social environments in local bars and restaurants. Users can explore the area around them to find the perfect watering hole.  They can go solo or create parties and place themselves on the map for others to find them. Create friends lists, plan events through private messages, check their areas for new locations or curate a list of their favorites, and even check population of all the participating businesses.  If you are a business owner, register with us and experience a new, fluid method of communication with your customers: alert them to promotions, popup events, crowd numbers, and keep them up to date.",
+    Tech: "React, React-Router, Node.js, Express, Axios, Socket.io, Google Maps API, Material UI, PostgreSQL, Google Maps, Google Translate, Webpack"
   },
   {
     Name: "Radma",
-    Image: "image",
+    Image: Radma,
     GitHub: "https://github.com/christophermbooth/Rad-Music-App",
-    Description: "Radma is a tool for those who love music - the grunge heads, the easy listeners, the old man down the street.  This radically interactive app lets you add your favorite local bands that others may not know of, search for new bands, create reminders for concerts and events, and tune into some amazing live streams from your favorite artists!"
+    Description: "Radma is a tool for those who love music - the grunge heads, the easy listeners, the old man down the street.  This radically interactive app lets you add your favorite local bands that others may not know of, search for new bands, create reminders for concerts and events, and tune into some amazing live streams from your favorite artists!",
+    Tech: "React, Node.js, Express.js, Axios, Webpack, Google Auth, Google Maps, MySQL, Sequalize, Twilio, Material UI, Digital Ocean"
   },
   {
     Name: "On The Road Again",
-    Image: "image",
+    Image: Prost,
     GitHub: "https://github.com/christophermbooth/OnTheRoadAgain",
-    Description: "On the Road is a tool for the especially adventurous road tripper who jumps at any chance to sleep outside and under the stars. Travelers upload information for their upcoming road trips, and in turn, On the Road provides them public campsites for each night they’ll be en route based on their desired daily drive time. After a trip has been created, travelers will gain access to a journal associated with that trip where they upload notes or photos as a way to preserve their fond memories."
+    Description: "On the Road is a tool for the especially adventurous road tripper who jumps at any chance to sleep outside and under the stars. Travelers upload information for their upcoming road trips, and in turn, On the Road provides them public campsites for each night they’ll be en route based on their desired daily drive time. After a trip has been created, travelers will gain access to a journal associated with that trip where they upload notes or photos as a way to preserve their fond memories.",
+    Tech: "Vue, Vuex, Node.js, Express, Axios, MySQL, Sequalize, Google Maps, Jest, Digital Ocean"
   },
   {
     Name: "ProgrammingBooth",
-    Image: "image",
+    Image: ProgrammingBooth,
     GitHub: "https://github.com/christophermbooth/portfolio",
-    Description: "A place to show off my work and personality. You're here right now!"
+    Description: "A place to show off my work and personality with as little reliance on third party libraries as possible. You're here right now!",
+    Tech: "React, React-Router, Node.js, Express, Dart-Sass, Axios, Nodemailer, Webpack"
   },
 ];
