bcde5aaea43bc9dfc0bf685b9a469c793ada42a4
Christopher Booth <61816572+christophermbooth@users.noreply.github.com>
2021-03-25 14:47:18 -0500

    (Modify) component to use appropriate photo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../styles/About.scss';
-import placeholder from '../images/placeholder.jpeg'
+import Chris from '../images/Chris.jpeg'
 
 const About = () => {
   return (
@@ -9,17 +9,17 @@ const About = () => {
         <div className="text-group">
           <h2>About me</h2>
           <p>Hi, I'm Chris! I am a software developer based in New Orleans, LA with an incredible passion for technology and how
-            far it can take us.  Growing up, I wanted to be an engineer so I tinkered with electronics.
-            I usually broke more than I fixed.
+          far it can take us.  Growing up, I wanted to be an engineer so I tinkered with electronics.
+          I usually broke more than I fixed.
           </p>
           <p>
             Since 2006, I've been engrossed in computers - both hardware and software.  I tore apart machines, rebuilt them, built new ones from scratch,
             played video games, modified video games, hosted 24/7 dedicated game servers, managed the home network to safely expose it to the internet, repurposed old commercial grade
-            server hardware for home use, spun up multiple services all in virtualized environments, and generally taught myself everything I know.  
+            server hardware for home use, spun up multiple services all in virtualized environments, and generally taught myself everything I know.
           </p>
           <p>
             I have over 5 years of experience with HTML and CSS
-            and over 2 years with JavaScript.  I recently graduated from Operation Spark in New Orleans as 
+            and over 2 years with JavaScript.  I recently graduated from Operation Spark in New Orleans as
             a full stack web developer with a certificate in IBC fundamentals of Javascript, functional programming,
             and web development level 3.  I learned a lot from Operation Spark and I'm excited to see what's next.
           </p>
@@ -29,7 +29,7 @@ const About = () => {
           </p>
         </div>
         <div className="cb-img">
-          <img src={placeholder} height="500px" />
+          <img src={Chris} height="500px" />
         </div>
       </div>
     </div>
