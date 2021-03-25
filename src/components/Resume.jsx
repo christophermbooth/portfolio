b3e8e497a94d807a738c5da0e2440155420fd1e9
Christopher Booth <61816572+christophermbooth@users.noreply.github.com>
2021-03-25 15:37:23 -0500

    (Style) Resume component

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import '../styles/Resume.scss';
 
 const Resume = () => {
   const resumeLink = "https://drive.google.com/file/d/1_9eRw68ve9jbRtBKm_LvQLkONEBqkIKa/preview"
   return (
     <div className="content">
-      <h3>My Resume</h3>
-      <embed src={resumeLink} width="100%" height="800px">
-      </embed>
+      <h2 className="resume-header">My Resume</h2>
+      <object data={resumeLink} className="resume-object">
+        It appears your browser is having trouble viewing the document.  <a href="https://drive.google.com/file/d/1_9eRw68ve9jbRtBKm_LvQLkONEBqkIKa/view?usp=sharing">Click here to download it.</a>
+      </object>
     </div>
   )
 }
